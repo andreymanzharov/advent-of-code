bf019fad74760516867138082d02d18c0e1dedc9
Andrey Manzharov <andreyrmg@gmail.com>
2021-12-01 08:39:24 +0300

    2021: day 01

---
+++ 2021/a01.cc
@@ -0,0 +1,79 @@
+#define DBG_MACRO_NO_WARNING
+#include "dbg.h"
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cctype>
+#include <charconv>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+namespace x {
+
+template <typename T> T to(string_view from) {
+  T r{};
+  if (auto [p, ec] = from_chars(from.data(), from.data() + from.size(), r);
+      p != from.data() + from.size()) {
+    ostringstream message;
+    message << "cannot convert " << from;
+    throw invalid_argument(message.str());
+  }
+  return r;
+}
+
+vector<string_view> split(string_view sv, string_view delimiters = ", "sv) {
+  vector<string_view> parts;
+  while (!sv.empty()) {
+    auto p = sv.find_first_of(delimiters);
+    if (p > 0)
+      parts.push_back(sv.substr(0, p));
+    if (p == string_view::npos)
+      break;
+    sv.remove_prefix(p + 1);
+  }
+  return parts;
+}
+} // namespace x
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int> d;
+  int x;
+  while (cin >> x) {
+    d.push_back(x);
+  }
+
+  {
+    int count = 0;
+    for (int i = 1; i < (int)d.size(); ++i) {
+      if (d[i] > d[i - 1])
+        count++;
+    }
+    cout << count << endl;
+  }
+
+  {
+    int count = 0;
+    for (int i = 3; i < (int)d.size(); ++i) {
+      if (d[i] > d[i - 3])
+        count++;
+    }
+    cout << count << endl;
+  }
+}
