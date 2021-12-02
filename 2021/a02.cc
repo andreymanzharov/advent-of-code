3c0aab0c2f25674ecdb35f5b73158c982410e68b
Andrey Manzharov <andreyrmg@gmail.com>
2021-12-02 08:20:07 +0300

    2021: day 02

---
+++ 2021/a02.cc
@@ -0,0 +1,84 @@
+#define DBG_MACRO_NO_WARNING
+#include "dbg.h"
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cctype>
+#include <charconv>
+#include <cmath>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+namespace x {
+
+template<typename T>
+T
+to(string_view from)
+{
+  T r{};
+  if (auto [p, ec] = from_chars(from.data(), from.data() + from.size(), r);
+      p != from.data() + from.size()) {
+    ostringstream message;
+    message << "cannot convert " << from;
+    throw invalid_argument(message.str());
+  }
+  return r;
+}
+
+vector<string_view>
+split(string_view sv, string_view delimiters = ", "sv)
+{
+  vector<string_view> parts;
+  while (!sv.empty()) {
+    auto p = sv.find_first_of(delimiters);
+    if (p > 0)
+      parts.push_back(sv.substr(0, p));
+    if (p == string_view::npos)
+      break;
+    sv.remove_prefix(p + 1);
+  }
+  return parts;
+}
+} // namespace x
+
+using namespace x;
+
+int
+main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int position = 0;
+  int depth = 0;
+  int aim = 0;
+
+  string line;
+  while (getline(cin, line)) {
+    auto v = split(line);
+    auto x = to<int>(v.back());
+    if (v.front().front() == 'f') {
+      position += x;
+      depth += aim * x;
+    } else if (v.front().front() == 'd') {
+      aim += x;
+    } else {
+      aim -= x;
+    }
+  }
+
+  cout << position * depth << endl;
+}
