a0f70d360d941cc4e034661c0be3a0cd9d447fc2
Andrey Manzharov <andreyrmg@gmail.com>
2021-12-22 17:55:05 +0300

    2021: not working solution for day 21

--- 2021/a21.cc
+++ 2021/a21.cc
@@ -80,5 +80,63 @@ main()
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
+  struct State
+  {
+    struct
+    {
+      int p, n;
+    } p1, p2;
+    bool first;
+
+    bool operator<(const State& other) const
+    {
+      return tuple{ p1.p, p1.n, p2.p, p2.n, first } < tuple{
+        other.p1.p, other.p1.n, other.p2.p, other.p2.n, other.first
+      };
+    }
+  };
+
+  map<State, pair<long, long>> d;
+  function<pair<long, long>(State)> Rec = [&](State s) {
+    if (s.p1.n >= 21)
+      return pair{ 1l, 0l };
+    if (s.p2.n >= 21)
+      return pair{ 0l, 1l };
+    pair<long, long> r{ 0, 0 };
+    for (int a = 1; a <= 3; ++a) {
+      for (int b = 1; b <= 3; ++b) {
+        for (int c = 1; c <= 3; ++c) {
+          auto x = a + b + c;
+          auto t{ s };
+          if (s.first) {
+            t.p1.p = (t.p1.p + x - 1) % 10 + 1;
+            t.p1.n += t.p1.p;
+          } else {
+            t.p2.p = (t.p2.p + x - 1) % 10 + 1;
+            t.p2.n += t.p2.p;
+          }
+          t.first = !s.first;
+          long w1, w2;
+          if (auto it = d.find(t); it != d.end()) {
+            w1 = it->second.first;
+            w2 = it->second.second;
+          } else {
+            auto p = Rec(t);
+            d.emplace(t, p);
+            w1 = p.first;
+            w2 = p.second;
+          }
+          r.first += w1;
+          r.second += w2;
+        }
+      }
+    }
+    return r;
+  };
+
+  auto [w1, w2] = Rec({ { 0, 9 }, { 0, 3 }, true });
+
+  cout << max(w1, w2) << endl;
+
   return 0;
 }
